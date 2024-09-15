afd31b6de384eb749de7a0bc8d30ce99566c5e4b
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-09-16 00:06:08 +0600

    added 2fa

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UseGuards,
+  Request,
+  Get,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserService } from 'src/users/user.service';
 import { User } from 'src/users/entities/user.entity';
 import { CreateUserDTO } from 'src/users/dto/create-user.dto';
 import { LoginDTO } from './dto/login.dto';
+import { JwtAuthGuard } from './jwt-guard';
+import { Enable2FAType } from './types';
+import { ValidateTokenDTO } from './dto/validate-token.dto';
+import { UpdateResult } from 'typeorm';
 
 @Controller('auth')
 export class AuthController {
@@ -26,5 +37,38 @@ export class AuthController {
     loginDTO: LoginDTO,
   ) {
     return this.authService.login(loginDTO);
+  }
+
+  @Get('enable-2fa')
+  @UseGuards(JwtAuthGuard)
+  enable2FA(
+    @Request()
+    req,
+  ): Promise<Enable2FAType> {
+    console.log(req.user);
+    return this.authService.enable2FA(req.user.userId);
+  }
+
+  @Post('validate-2fa')
+  @UseGuards(JwtAuthGuard)
+  validate2FA(
+    @Request()
+    req,
+    @Body()
+    ValidateTokenDTO: ValidateTokenDTO,
+  ): Promise<{ verified: boolean }> {
+    return this.authService.validate2FAToken(
+      req.user.userId,
+      ValidateTokenDTO.token,
+    );
+  }
+
+  @Get('disable-2fa')
+  @UseGuards(JwtAuthGuard)
+  disable2FA(
+    @Request()
+    req,
+  ): Promise<UpdateResult> {
+    return this.authService.disable2FA(req.user.userId);
   }
 }
--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -3,7 +3,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { Repository } from 'typeorm';
+import { Repository, UpdateResult } from 'typeorm';
 import * as bcrypt from 'bcryptjs';
 import { LoginDTO } from 'src/auth/dto/login.dto';
 
@@ -31,6 +31,30 @@ export class UserService {
       throw new UnauthorizedException('Could not find user');
     }
     return user;
+  }
+
+  async findById(id: number): Promise<User> {
+    return this.userRepository.findOneBy({ id: id });
+  }
+
+  async updateSecretKey(userId, secret: string): Promise<UpdateResult> {
+    return this.userRepository.update(
+      { id: userId },
+      {
+        twoFASecret: secret,
+        enable2FA: true,
+      },
+    );
+  }
+
+  async disable2FA(userId: number): Promise<UpdateResult> {
+    return this.userRepository.update(
+      { id: userId },
+      {
+        enable2FA: false,
+        twoFASecret: null,
+      },
+    );
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
